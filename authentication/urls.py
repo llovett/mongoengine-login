c1ce96226f21412dfd31e555a6fc20421ead9b45
Luke Lovett <llovett@oberlin.edu>
2013-01-17 00:32:02 -0500

    working on 'login with your google account' successfully finding google authentication endpoint can parse response from google!

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,8 @@ from gauth import views
 urlpatterns = patterns('',
                        url( r'^$', views.user_show, name="main_page" ),
                        url( r'^accounts/login/$', views.login_view, name="login_url" ),
+                       url( r'^accounts/google/login/$', views.google_login, name="google_login_url" ),
+                       url( r'^accounts/google/success/$', views.google_login_success, name="google_login_success" ),
                        url( r'^accounts/profile/$', views.user_show, name="login_success" ),
                        url( r'^accounts/logout/$', views.user_logout, name="logout_url" ),
                        url( r'^accounts/register/$', views.register, name="register_url" ),
