d21e25bc2d93c027892927ee7b91257193b270aa
Luke Lovett <llovett@oberlin.edu>
2013-01-17 14:52:51 -0500

    have userprofile objects being made

--- gauth/models.py
+++ gauth/models.py
@@ -2,9 +2,31 @@ import mongoengine as mdb
 from random import choice
 from datetime import datetime
 
-class UserLoginStub( mdb.Document ):
+class OpenidAuthStub( mdb.EmbeddedDocument ):
     '''
-    Models a User about to login
+    Stores information for OpenID transactions for a single user
+
+    '''
+    # Association: good for one session
+    assocation = mdb.StringField()
+    # Claimed ID: this will always uniquely identify this user, as long as our realm doesn't change
+    claimed_id = mdb.StringField()
+
+class UserProfile( mdb.Document ):
+    '''
+    Stores additional information for Users
+    
+    '''
+    user = mdb.ReferenceField( 'mongoengine.django.auth.User' )
+    openid_auth_stub = mdb.EmbeddedDocumentField( OpenidAuthStub )
+
+    def __unicode__( self ):
+        return '{} (profile)'.format( self.user.username )
+
+class RegistrationStub( mdb.Document ):
+    '''
+    Contains information necessary during registration process
+    
     '''
     user = mdb.ReferenceField( 'mongoengine.django.auth.User' )
     activationCode = mdb.StringField( max_length=100 )
@@ -21,4 +43,4 @@ class UserLoginStub( mdb.Document ):
     # No more than 10,000 documents,
     # No more than 20,000,000 bytes (20 MB)
     meta = { 'max_documents':10000, 'max_size':20000000 }
-        
+
